bfbe5fdfeb5c001e92d9c5f9b2da983848abda8e
KafuiPraise <matildapraise@gmail.com>
2024-03-08 05:07:39 +0000

    UFT

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""utf-8 validation module"""
+
+
+def validUTF8(data):
+    """
+    Determine if the given data set represents a valid UTF-8 encoding.
+
+    Args:
+        data (List[int]): The list of integers representing the data.
+
+    Returns:
+        bool: True if data is a valid UTF-8 encoding, else False.
+    """
+    num_bytes = 0
+
+    for byte in data:
+        byte = byte & 0xFF
+
+        if num_bytes == 0:
+            if byte >> 5 == 0b110:
+                num_bytes = 1
+            elif byte >> 4 == 0b1110:
+                num_bytes = 2
+            elif byte >> 3 == 0b11110:
+                num_bytes = 3
+            elif byte >> 7 == 0b0:
+                continue
+            else:
+                return False
+        else:
+            if byte >> 6 != 0b10:
+                return False
+            num_bytes -= 1
+
+    return num_bytes == 0
